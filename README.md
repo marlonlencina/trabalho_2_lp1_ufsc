b208e408dc3da3fb59f8af8c17a2902506898c7d
Marlon Lencina <marlonlenciina@gmail.com>
2026-06-26 20:30:35 -0300

    Added HTML

--- README.md
+++ README.md
@@ -4,11 +4,11 @@
 [X] [Marlon] - 20/06/2026 - Crud de Plantas, Controle global de estados, dados via Struct no Main, UX melhorada, todas funcoes de relatorio e pesquisa.  
 [X] [Marlon] - 21/06/2026 - Count functions, update range function, fix null errors and segmentation faults, persistência de dados
 
-[] Exportar para arq. texto .csv  
-[] Exportar para arq. texto .html   
+[X] [Joao] Exportar para arq. texto .csv  
+[X] [Joao] Exportar para arq. texto .html   
 [] Funçao recursiva  
 [] compilar codigo a partir de um Makefile  
-[] desacoplar o main em arquivos .h e .c 
+[X] desacoplar o main em arquivos .h e .c 
 
 
 By marlon e joao (Also know as Xaropinho)  
--- v_marlon.c
+++ v_marlon.c
@@ -1734,66 +1734,22 @@ void generateReportOfInspectionsVariation(t_location *locations)
 void generateCSVFile(t_app_state *app_state)
 {
     FILE *fp_locations = NULL;
-    FILE *fp_sectors = NULL;
-    FILE *fp_sensors = NULL;
-    FILE *fp_inspections = NULL;
 
     string filepath_locations;
     strcpy(filepath_locations, getFilepathMatchEntity(LOCATION, app_state->filepath_csv).response);
     fp_locations = fopen(filepath_locations, "w");
-    string filepath_sectors;
-    strcpy(filepath_sectors, getFilepathMatchEntity(SECTOR, app_state->filepath_csv).response);
-    fp_sectors = fopen(filepath_sectors, "w");
-    string filepath_sensors;
-    strcpy(filepath_sensors, getFilepathMatchEntity(SENSOR, app_state->filepath_csv).response);
-    fp_sensors = fopen(filepath_sensors, "w");
-    string filepath_inspections;
-    strcpy(filepath_inspections, getFilepathMatchEntity(INSPECTION, app_state->filepath_csv).response);
-    fp_inspections = fopen(filepath_inspections, "w");
 
     fprintf(fp_locations, "id; nome\n");
-    fprintf(fp_sectors, "id; nome; id da planta; descricao\n");
-    fprintf(fp_sensors, "id; nome; id do setor; tipo do sensor\n");
-    fprintf(fp_inspections, "id; id do sensor; Valor da inpecao; Data da inspecao\n");
 
     t_location *current_location = app_state->database;
     while (current_location != NULL)
     {
-        printf("ID: %s - Exportado para .csv\n", current_location->id);
+        printf("ID: %s - Exportado para .csv.", current_location->id);
         fprintf(fp_locations, "%s; %s\n", current_location->id, current_location->name);
-
-        t_sector *current_sector = current_location->sectors;
-
-        while (current_sector != NULL)
-        {
-            printf("ID: %s - Exportado para .csv\n", current_sector->id);
-            fprintf(fp_sectors, "%s; %s; %s; %s\n", current_sector->id, current_sector->name,
-                    current_sector->location_id, current_sector->description);
-            t_sensor *current_sensor = current_sector->sensors;
-            while (current_sensor != NULL)
-            {
-                printf("ID: %s - Exportado para .csv\n", current_sensor->id);
-                fprintf(fp_sensors, "%s; %s; %s; %i\n", current_sensor->id, current_sensor->name,
-                        current_sensor->sector_id, current_sensor->sensor_type);
-                t_inspection *current_inspection = current_sensor->inspections;
-                while (current_inspection != NULL)
-                {
-                    printf("ID: %s - Exportado para .csv\n", current_inspection->id);
-                    fprintf(fp_inspections, "%s; %s; %f; %li\n", current_inspection->id, current_inspection->sensor_id,
-                            current_inspection->value, current_inspection->date_inspection);
-                    current_inspection = current_inspection->next;
-                }
-                current_sensor = current_sensor->next;
-            }
-            current_sector = current_sector->next;
-        }
         current_location = current_location->next;
     }
 
     fclose(fp_locations);
-    fclose(fp_sectors);
-    fclose(fp_sensors);
-    fclose(fp_inspections);
 
     printf("Arquivo (%s) gerado com sucesso. \n", filepath_locations);
 }
@@ -1803,10 +1759,7 @@ void generateHTMLFile(t_app_state *app_state)
 
     fp = fopen(app_state->filepath_html, "w");
 
-    fprintf(fp, "<!DOCTYPE html>\n");
-    fprintf(fp, "<head><meta charset="
-                "UTF-8"
-                "></head>\n");
+    fprintf(fp, "<html>\n");
     fprintf(fp, "<body>\n");
 
     fprintf(fp, "<table>\n");
@@ -1834,7 +1787,7 @@ void generateHTMLFile(t_app_state *app_state)
     }
     fprintf(fp, "</tbody>\n");
     fprintf(fp, "</table>\n");
-    fprintf(fp, "<br><br><br>\n");
+
     fprintf(fp, "<table>\n");
     fprintf(fp, "<caption>Tabela de setores</caption>");
 
@@ -1871,7 +1824,7 @@ void generateHTMLFile(t_app_state *app_state)
 
     fprintf(fp, "</tbody>\n");
     fprintf(fp, "</table>\n");
-    fprintf(fp, "<br><br><br>\n");
+
     fprintf(fp, "<table>\n");
     fprintf(fp, "<caption>Tabela de sensores</caption>");
 
@@ -1913,56 +1866,6 @@ void generateHTMLFile(t_app_state *app_state)
         }
 
         current_location_sensors = current_location_sensors->next;
-    }
-
-    fprintf(fp, "</tbody>\n");
-    fprintf(fp, "</table>\n");
-    fprintf(fp, "<br><br><br>\n");
-
-    fprintf(fp, "<table>\n");
-    fprintf(fp, "<caption>Tabela de leitura de sensores</caption>");
-
-    fprintf(fp, "<thead>\n");
-    fprintf(fp, "<tr>\n");
-    fprintf(fp, "<th>Id</th>\n");
-    fprintf(fp, "<th>Id do sensor</th>\n");
-    fprintf(fp, "<th>Valor da leitura</th>\n");
-    fprintf(fp, "<th>Data da leitrua</th>\n");
-    fprintf(fp, "</tr>\n");
-    fprintf(fp, "</thead>\n");
-    fprintf(fp, "<tbody>\n");
-
-    t_location *current_location_inspections = app_state->database;
-    while (current_location_inspections != NULL)
-    {
-        t_sector *current_sensors_inspections = current_location_inspections->sectors;
-
-        while (current_sensors_inspections != NULL)
-        {
-            t_sensor *sensor_inspections = current_sensors_inspections->sensors;
-
-            while (sensor_inspections != NULL)
-            {
-                t_inspection *current_inspection = sensor_inspections->inspections;
-
-                while (current_inspection != NULL)
-                {
-
-                    fprintf(fp, "<tr>");
-                    fprintf(fp, "<td>%s</td>", current_inspection->id);
-                    fprintf(fp, "<td>%s</td>", current_inspection->sensor_id);
-                    fprintf(fp, "<td>%f</td>", current_inspection->value);
-                    fprintf(fp, "<td>%li</td>", current_inspection->date_inspection);
-                    fprintf(fp, "</tr>");
-
-                    current_inspection = current_inspection->next;
-                }
-                sensor_inspections = sensor_inspections->next;
-            }
-            current_sensors_inspections = current_sensors_inspections->next;
-        }
-
-        current_location_inspections = current_location_inspections->next;
     }
 
     fprintf(fp, "</tbody>\n");
